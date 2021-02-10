f63112b4a07babf278138259db06d8bd534a7dbc
Matias Gadea <matiasegadea@gmail.com>
2021-02-09 23:18:15 -0300

    5ta entrega: promise

--- src/Components/Containers/ItemListContainer.jsx
+++ src/Components/Containers/ItemListContainer.jsx
@@ -1,11 +1,28 @@
+import React, {useState, useEffect} from 'react';
 import './ItemListContainer.css';
+import ItemList from '../../Components/ItemList/ItemList';
+import InfoProductos from '../../mocks/productList';
+
 
 const ItemListContainer = ({greeting}) => {
-    
+
+    const [products, setProducts] = useState([]);
+    useEffect (() => {
+        const myPromise = new Promise ((resolve, reject) => {
+            setTimeout(() => resolve(InfoProductos), 3000);
+        });
+
+        myPromise.then((result) => setProducts(result));
+    }, []);
+
+
     return (
-        <div className="container-fluid" id="Home"> {greeting} </div>
-    )
-}
+        <div>
+            <h2 className="container-fluid" id="Home"> {greeting} </h2>
+            <ItemList products={products} />
+        </div>
+    );
+};
 
 
 
---
+++ src/Components/Item/Item.jsx
@@ -0,0 +1,14 @@
+const Item = ({ product }) => {
+    return (
+        <div className="col-4 text-center">
+            <img src={product.image} className="img-fluid" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">{product.title}</h5>
+                <p className="card-text">{product.price}</p>
+                <a href="#" className="btn btn-primary">Agregar al Carrito</a>
+            </div>
+        </div> 
+    );
+};   
+
+export default Item;
